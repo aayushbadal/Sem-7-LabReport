6f78ebbc3ce709e2705964287a96d3e5f4207720
aayushbadal <aayushbadal922@gmail.com>
2026-02-27 17:48:44 +0545

    Java Lab1 Complete

--- ADJ/src/Lab1/Q7.java
+++ ADJ/src/Lab1/Q7.java
@@ -3,5 +3,22 @@
 package Lab1;
 
 public class Q7 {
+    public int add(int a, int b) {
+        return a + b;
+    }
 
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Q7 mtd_overl = new Q7();
+        System.out.println("Sum of Two integer =" + mtd_overl.add(5, 6));
+        System.out.println("Sum of three integer = " + mtd_overl.add(5, 6, 7));
+        System.out.println("Sum of two double = " + mtd_overl.add(5.5, 6.5));
+    }
 }
